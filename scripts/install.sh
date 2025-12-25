73398034627758b3a1ded79120283287bd0f2798
Dmitry Sokolov <xx.sokolov@mail.ru>
2025-12-25 17:11:09 +0300

    +

--- scripts/install.sh
+++ scripts/install.sh
@@ -15,8 +15,8 @@ fi
 echo "[1/5] Обновление списка пакетов opkg..."
 opkg update
 
-echo "[2/5] Установка минимально необходимых пакетов..."
-opkg install dnsmasq-full ipset shadowsocks-libev-ss-redir shadowsocks-libev-config
+echo "[2/5] Установка и переустановка пакетов..."
+opkg install --force-reinstall dnsmasq-full ipset shadowsocks-libev-ss-redir shadowsocks-libev-config
 
 # --- 2. Создание структуры директорий и файлов ---
 echo "[3/5] Создание структуры директорий и файлов..."
